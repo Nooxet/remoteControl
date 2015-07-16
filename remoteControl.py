9487ac99e587de8b746acb161f496d6d622a94e4
Jonathan Sönnerup <Nooxet@gmail.com>
2015-07-17 00:46:04 +0200

    added main method so we can start, stop and restart the daemon. not redirecting stdout,stderr due to debugging

--- remoteControl.py
+++ remoteControl.py
@@ -17,12 +17,12 @@ import SocketServer
 
 from daemon import Daemon
 
-class remoteControl(SocketServer.ThreadingTCPServer, Daemon):
+class RemoteControl(SocketServer.ThreadingTCPServer, Daemon):
 	
 	PORT = 1337
 
 	def __init__(self, port=1337):
-		Daemon.__init__(self, '/var/run/remoteControl.pid', stderr = '/var/log/remoteControl.errlog')
+		Daemon.__init__(self, '/var/run/remoteControl.pid', stdout='/dev/stdout', stderr='/dev/stderr')#, stderr = '/var/log/remoteControl.errlog')
 		self.PORT = port
 		self.log('Remote Control server started at port %d' % self.PORT)
 
@@ -39,6 +39,31 @@ class remoteControl(SocketServer.ThreadingTCPServer, Daemon):
 		sys.stderr.write(time.asctime(time.localtime(time.time())) + ': ')
 		sys.stderr.write(msg.rstrip() + '\n')
 
+
+class RCHandler(SocketServer.BaseRequestHandler):
+
+	def setup(self):
+		pass
+
+	def handle(self):
+		pass
+
 if __name__ == '__main__':
-	rc = remoteControl()
-	rc.start()
+	rc = RemoteControl()
+
+	if len(sys.argv) == 2 or len(sys.argv) == 3:
+		if sys.argv[1] == 'start':
+			rc.start()
+		elif sys.argv[1] == 'stop':
+			rc.stop()
+		elif sys.argv[1] == 'restart':
+			rc.restart()
+		elif sys.argv[1] == 'nod': # no-daemon mode
+			rc.run()
+		else:
+			print 'Unknown command'
+			sys.exit(2) # command line error		
+		sys.exit(0)
+	else:
+		print 'Usage: %s start|stop|restart|nod' % sys.argv[0]
+		sys.exit(2)
