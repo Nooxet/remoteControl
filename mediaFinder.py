fe3d5f04f5adc72a96069c2dc5f791fbd4d25374
Habban <andreasrydell_91@hotmail.com>
2015-10-15 22:52:22 +0200

    optimized the code, wip

--- mediaFinder.py
+++ mediaFinder.py
@@ -7,35 +7,35 @@ class MediaFinder:
 	def __init__(self, path = '.'):
 		self.path = path
 		self.movieext = [".mp4", ".mkv", ".avi"]
-		self.movies = []
 		self.musicext = [".mp3", ".wma", ".flac"]
-		self.music = []
-		
 
-	def get_movies(self):
+
+	def get_media(self, mediaext):
 		"""
-		Returns a list with all movie files
+		Returns a list with all media files
 		"""
+		medialist = []
 		for root, dirs, files in os.walk(self.path):
 			for file in files:
 				name, ext = os.path.splitext(file)
-				if ext in self.movieext:
-					self.movies.append(name)
-		
-		return self.movies
+				if ext in mediaext:
+					medialist.append(root + "\\" +  file)
+
+		return medialist
 		
 	def get_music(self):
 		"""
 		Returns a list with all music files
 		"""
-		for root, dirs, files in os.walk(self.path):
-			for file in files:	
-				name, ext = os.path.splitext(file)
-				if ext in self.musicext:
-					self.music.append(name)
-						
-		return self.music
-			
+		return self.get_media(self.musicext)
+		
+	def get_movies(self):
+		"""
+		Returns a list with all movies files
+		"""
+		return self.get_media(self.movieext)
+
+
 		
 					
 			
@@ -45,11 +45,12 @@ class MediaFinder:
 
 if __name__ == '__main__':
 	# create a media finder object, with path = 'mypath'
-	mf = MediaFinder('C:\\Users\\Andreas\\Desktop\\')
+	mf = MediaFinder('C:\\Users\\Andreas\\Desktop')
 
 	# prints the list with all media
 	# print mf.get_movies()
-	print mf.get_music()
+	print "music:", mf.get_music()
+	print "filmer:", mf.get_movies()
 	
 	
 	
